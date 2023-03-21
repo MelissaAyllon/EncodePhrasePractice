80906996eb8140466af197d0fa40b84f7f6b0f81
MelissaAyllon <mayllon@espol.edu.ec>
2023-03-20 23:11:30 -0500

    Avance del metodo "decode"

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        RunLengthEncoding nom = new RunLengthEncoding();
+        System.out.println(nom.encode("MelisSa"));
+    }
+}
